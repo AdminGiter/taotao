69e1051fd2fc0354c60e6772d3636ecf20456b2a
Gentleman <chenptfirst@126.com>
2018-01-02 17:36:46 +0800

    商品常量类

---
+++ taotao/taotao-common/src/main/java/com/taotao/common/constance/ItemConstance.java
@@ -0,0 +1,22 @@
+package com.taotao.common.constance;
+
+/**
+ * 商品常量类
+ */
+public class ItemConstance {
+
+    /**
+     * 正常
+     */
+    public static final String ITEM_STAUS_NORMAIL="1";
+
+    /**
+     * 下架
+     */
+    public static final String ITEM_STAUS_UNDERCARRIAGE="2";
+
+    /**
+     * 删除
+     */
+    public static final String ITEM_STAUS_DELETE="3";
+}
